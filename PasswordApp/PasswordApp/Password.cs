57540a4b0fbc446cc7ab481043dd67fd38332102
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-07-05 20:16:45 +0400

    initialization of the Password class

---
+++ PasswordApp/PasswordApp/Password.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordApp
+{
+    public class Password
+    {
+        private char _Letter { get; set; }
+        private char _Symbol { get; set; }
+        private int _Digit { get; set; }
+
+        private string GeneratedPassword { get; set; }
+
+       public Password (char Letter, char Symbol, int Digit)
+        {
+            _Letter = Letter;
+            _Symbol = Symbol;
+            _Digit = Digit;
+            GeneratedPassword = string.Concat(_Letter, _Symbol, _Digit);
+        }
+         public string GetPassword()
+        {
+            return GeneratedPassword;
+        }
+
+    }
+}
+/*приложение для генерации пароля по заданному шамблону: количество букв, спец. символов, цифр, общая длина пароля
+
+Каждая генерация - это отдельный поток*/
---
+++ PasswordApp/PasswordApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PasswordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Password password = new Password('S','^', 1);
+
+            Console.WriteLine(password.GetPassword());
+
+            Console.ReadKey();
+
+        }
+    }
+}
