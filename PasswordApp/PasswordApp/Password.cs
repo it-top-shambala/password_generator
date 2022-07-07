e830a2652a40257d31b91689e6992d7c33c381d5
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-07-07 22:19:12 +0400

    class Password : running password generation threads

--- PasswordApp/PasswordApp/Password.cs
+++ PasswordApp/PasswordApp/Password.cs
@@ -1,29 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace PasswordApp
 {
     public class Password
     {
-        private char _Letter { get; set; }
-        private char _Symbol { get; set; }
-        private int _Digit { get; set; }
+        private string password { get; set; }
 
-        private string GeneratedPassword { get; set; }
-
-       public Password (char Letter, char Symbol, int Digit)
+        public Password ()
         {
-            _Letter = Letter;
-            _Symbol = Symbol;
-            _Digit = Digit;
-            GeneratedPassword = string.Concat(_Letter, _Symbol, _Digit);
-        }
-         public string GetPassword()
-        {
-            return GeneratedPassword;
+            password = "";
         }
 
+        public string SetPassword (string value)
+        {
+            return string.Concat(password, value);
+        }
+
+        public void PasswordGeneration(int numberLetters, int numberSymbols, int numberDigits)
+        {
+           
+            Task[] taskLetters = new Task[numberLetters];
+            for (var i = 0; i < numberLetters; i++)
+            {
+                taskLetters[i] = Task.Factory.StartNew(() =>
+                {
+                    Letter symbol = new Letter();
+                    symbol.Generating();
+                    password = SetPassword( symbol.CodeSumbol.ToString());
+                    Console.WriteLine($"Letter = {symbol.CodeSumbol} / Password - {password} ");
+
+                });
+            }
+        
+            Task[] taskSymbols = new Task[numberSymbols];
+            for (var i = 0; i < numberSymbols; i++)
+            {
+                taskSymbols[i] = Task.Factory.StartNew(() =>
+                {
+                    Special symbol = new Special();
+                    symbol.Generating();
+                    password = SetPassword(symbol.CodeSumbol.ToString());
+                    Console.WriteLine($"Special symbol = {symbol.CodeSumbol} / Password - {password}");
+                });
+                
+            }
+            Task[] taskDigits = new Task[numberDigits];
+            for (var i = 0; i < numberDigits; i++)
+            {
+                taskDigits[i] = Task.Factory.StartNew(() =>
+                {
+                    Digit symbol = new Digit();
+                    symbol.Generating();
+                    password = SetPassword(symbol.CodeSumbol.ToString());
+                    Console.WriteLine($"Digit = {symbol.CodeSumbol} / Password - {password}");
+                });
+            }
+           
+        }
     }
 }
 /*приложение для генерации пароля по заданному шамблону: количество букв, спец. символов, цифр, общая длина пароля
