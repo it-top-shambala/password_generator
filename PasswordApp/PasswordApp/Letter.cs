3ccd2554b29210854a243d1eabf7d2ce14ea4ddd
GasiliiGL <vasiliiglavatskih@gmail.com>
2022-07-07 22:16:05 +0400

    classes for generating character code

---
+++ PasswordApp/PasswordApp/Letter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordApp
+{
+    public class Letter: Symbol
+    {
+        readonly Random x = new Random();
+        public override void Generating()
+        {      
+            CodeSumbol = (char) x.Next(65, 90);
+        }
+    }
+}
---
+++ PasswordApp/PasswordApp/Symbol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordApp
+{
+    abstract public class Symbol
+    {
+        public char CodeSumbol;
+        public abstract void Generating();
+
+    }
+}
